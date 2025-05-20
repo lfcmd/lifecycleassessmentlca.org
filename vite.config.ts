d9a803747867b038bbe38ef9280da20f44a7a1a7
lfcmd66 <1783376585@qq.com>
2025-05-20 22:59:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// In vite.config.ts
+export default defineConfig({
+  base: '/',  // Change this if your site will be in a subdirectory
+  // other config...
+})
